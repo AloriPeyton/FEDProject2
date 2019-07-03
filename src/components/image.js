6ba782f80c38870ec85bace38a2131f85054dc76
AloriPeyton <aloripeyton@yahoo.com>
2019-07-03 14:16:33 -0500

    Update image.js

--- src/components/image.js
+++ src/components/image.js
@@ -18,7 +18,7 @@ const Image = () => {
     query {
       placeholderImage: file(relativePath: { eq:"testImage2.png"}) {
         childImageSharp {
-          fluid(maxWidth: 300) {
+          fluid(maxWidth: auto) {
             ...GatsbyImageSharpFluid
           }
         }
