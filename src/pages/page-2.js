a44cdd3cf90d5047a41b8c939f14a9a1885e165a
AloriPeyton <aloripeyton@yahoo.com>
2019-07-09 09:53:00 -0500

    Update page-2.js

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -7,8 +7,27 @@ import SEO from "../components/seo"
 const SecondPage = () => (
   <Layout>
     <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
+    <h1>Current Learning</h1>
+    <h2>Udemy</h2>
+    <ul>
+      <li>Automation Architect - Selenium Webdriver - 7 Live Projects</li> 
+      <li>Hello React - React Training for JavaScript Beginners</li>
+      <li>Selenium WebDriver + Java Complete Step by Step Course</li>
+    </ul>
+   <h2>Your Learning Courses</h2>
+    <ul>
+      <li>Selenium and Cucumber Automation Test TOol Training for Technical Professionals</li> 
+      <li>Combinatorial Test Design (CTD)</li>
+      <li>Software Testing Foundations: Test Planning and Management</li>
+    </ul>
+    <h2>Your Learning Courses</h2>
+    <ul>
+      <li>Selenium and Cucumber Automation Test TOol Training for Technical Professionals</li> 
+      <li>Combinatorial Test Design (CTD)</li>
+      <li>Software Testing Foundations: Test Planning and Management</li>
+    </ul>
+  
+    <p></p>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
