a9035dad3bb439bc508be3ed013f36f1fe94ff2f
AloriPeyton <aloripeyton@yahoo.com>
2019-07-10 12:39:48 -0500

    Update page-3.js

--- src/pages/page-3.js
+++ src/pages/page-3.js
@@ -7,45 +7,16 @@ import SEO from "../components/seo"
 const ThirdPage = () => (
   <Layout>
     <SEO title="Page Three" />
-    <h1>Current Learning</h1>
-    <h2>Udemy</h2>
-    <ul>
-      <li>Automation Architect - Selenium Webdriver - 7 Live Projects</li> 
-      <li>Hello React - React Training for JavaScript Beginners</li>
-      <li>Selenium WebDriver + Java Complete Step by Step Course</li>
-    </ul>
-   <h2>Your Learning Courses</h2>
-    <ul>
-      <li>Selenium and Cucumber Automation Test Tool Training for Technical Professionals</li> 
-      <li>Combinatorial Test Design (CTD)</li>
-      <li>Software Testing Foundations: Test Planning and Management</li>
-    </ul>
-    <h2>Badges Earned</h2>
-    <table>
-      <tr>
-      <td>IBM Agile Explorer<img src={require('../images/AA-Badge-explorer-352.png')}height="100" width="100" align="right"/></td> 
-      </tr>
-      <tr>                                
-      <td>IBM Automation Practitioner<img src={require('../images/Automation+Practitioner.png')}height="100" width="100" align="right"/></td>                           
-      </tr>
-      <tr>
-      <td>Data Science Foundations - Level 1<img src={require('../images/Data+Sci+Found+Level+1+-+CC+-+2019.png')}height="100" width="100" align="right"/></td>
-      </tr>
-      <tr>
-      <td>IGNITE Membership<img src={require('../images/IBM+IGNITE+Membership_281_29.png')}height="100" width="100" align="right"/></td>     
-      </tr>
-      <tr>
-      <td>IBM Cloud Fundamentals<img src={require('../images/IBM+Cloud+Essentials+-+Knowledge+Badge.png')}height="100" width="100" align="right"/></td> 
-      </tr>
-      <tr>
-      <td>IBM Blockchain Essentials<img src={require('../images/Blockchain+Essentials.png')}height="100" width="100"align="right"/></td>      
-      </tr>
-    </table>
-  
+    <h1>Under Construction...</h1>
+    
     <p></p>
-    <Link to="/">Go back to the homepage</Link>
+<table>
+<tr>        
+   <td> <Link to="/"align="left">Go back to the homepage</Link> </td>
+    <td><Link to="/page-2" align="right">Go to page 2</Link></td>
+</tr>
+</table>    
 
- <Link to="/page-2" align="right">Go to page 2</Link>
   </Layout>
 )
 
